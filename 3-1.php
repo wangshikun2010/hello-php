661c2f02fad2663e937ace47b679455b8d28aea6
wangshikun2010@gmail.com <wangshikun2010@gmail.com@5fe8941e-b0ba-6a15-4a2c-f5e8326fd213>
2012-09-10 15:20:36 +0000

    php中的常量使用
    git-svn-id: https://my-php-mysql.googlecode.com/svn/trunk@3 5fe8941e-b0ba-6a15-4a2c-f5e8326fd213

---
+++ 3-1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>php中常量的定义和使用</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<link href="" rel="stylesheet">
+</head>
+<body>
+    <?php
+    	define("name","valueAS",true);
+    	echo name."<br/>";
+    	echo NAME."<br/>";
+
+    	define("message","this is a define",false);
+    	echo message."<br/>";
+    	echo MESSAGE."<br/>";
+
+    	$test = "message";
+    	echo constant($test)."<br/>";
+    	echo $test."<br/>";
+    	echo defined("name");
+    ?>
+</body>
+</html>
---
+++ 3-2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>php中预定义常量的的使用</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="">
+	<link href="" rel="stylesheet">
+</head>
+<body>
+    <?php
+    	echo "所使用的文件名是：";
+    	echo _FILE_;
+    	echo "<br>";
+    	echo "文件的行数：";
+    	echo _LINE_;
+    	echo "<br>";
+    	echo "php的版本：";
+    	echo PHP_VERSION;
+    	echo "<br>";
+    	echo "所使用的操作系统的文件名：";
+    	echo PHP_OS;
+    ?>
+</body>
+</html>

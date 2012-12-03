27a81046105ed6080505fc7796323cbf77e94e63
wangshijun2010@gmail.com <wangshijun2010@gmail.com@5fe8941e-b0ba-6a15-4a2c-f5e8326fd213>
2012-12-03 12:54:27 +0000

    basic api framework, handlebar js as client side template engine
    git-svn-id: https://my-php-mysql.googlecode.com/svn/trunk@72 5fe8941e-b0ba-6a15-4a2c-f5e8326fd213

---
+++ projects/evernote-shijun/api.php
@@ -0,0 +1,44 @@
+<?php
+
+// Application Public Interface
+require 'config.php';
+
+$class = 'notebooks';
+$method = 'list';
+$response = array(
+	'data' => null,
+	'code' => null,
+	'status' => true,
+);
+
+if (isset($_GET['class'])) {
+	$class = trim($_GET['class']);
+	unset($_GET['class']);
+}
+
+if (isset($_GET['method'])) {
+	$method = trim($_GET['method']);
+	unset($_GET['method']);
+}
+
+$request = array_merge($_GET, $_POST);
+
+if (file_exists(CLASS_DIR . $class . '.php')) {
+	include CLASS_DIR . $class . '.php';
+
+	$object = new $class($request, $response);
+	if (method_exists($object, $method)) {
+		call_user_func(array($object, $method));
+		$response = $object->response;
+	} else {
+		$response['code'] = 'Method not found';
+		$response['status'] = false;
+	}
+
+} else {
+	$response['code'] = 'Class not found';
+	$response['status'] = false;
+}
+
+header('Content-type: application/json');
+echo json_encode($response);

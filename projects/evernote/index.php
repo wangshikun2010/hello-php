4e78ac5808f0b9e22d69239a00bfc74ec4c56f61
wangshikun2010@gmail.com <wangshikun2010@gmail.com@5fe8941e-b0ba-6a15-4a2c-f5e8326fd213>
2012-12-05 03:17:46 +0000

    evernote第三步
    git-svn-id: https://my-php-mysql.googlecode.com/svn/trunk@74 5fe8941e-b0ba-6a15-4a2c-f5e8326fd213

--- projects/evernote/index.php
+++ projects/evernote/index.php
@@ -15,60 +15,24 @@
 	<link type="text/css" rel="stylesheet" href="./css/reset.css">
 </head>
 <body>
-<!--<div class="nav">
-		<div class="nav-inner">
-			<ul>
-				<li class="li-1">Evernote</li>
-				<li class="li-2"><a href="" title="Evernote">首页</a></li>
-				<li class="li-3">
-					<div class="btn-group">
-						<button class="btn btn-small">Small action</button>
-						<button class="btn btn-small dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></button>
-						<ul class="dropdown-menu">
-							<li><a href="#">Action</a></li>
-							<li><a href="#">Another action</a></li>
-							<li><a href="#">Something else here</a></li>
-							<li class="divider"></li>
-							<li><a href="#">Separated link</a></li>
-						</ul>
-					</div>
-				</li>
-			</ul>
-		</div>
-	</div>
- -->
 	<div class="navbar">
 		<div class="navbar-inner">
 			<div class="container">
-				<a class="btn btn-navbar" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-				</a>
-				<a class="brand" href="#">Evernote</a>
-				<div class="nav-collapse collapse navbar-responsive-collapse">
-					<ul class="nav">
-						<li class="active"><a href="#">首页</a></li>
-					</ul>
-					<ul class="nav pull-right">
-						<li><a href="#">设置</a></li>
-						<li class="divider-vertical"></li>
-						<li><a href="#">用户</a></li>
-					</ul>
-				</div><!-- /.nav-collapse -->
+				<img class="img1" src="./img/gajgja.jpg" title="Evernote"/>
+				<ul class="nav">
+					<li><a class="active" href="">首页</a></li>
+				</ul>
+				<div class="pull-right">
+					<form class="form-search">
+						<div class="input-append">
+							<input type="text" class="span2 search-query">
+							<button type="submit" class="btn">Search</button>
+						</div>
+						<button type="button" class="btn new-btn">新建笔记</buttom>
+					</form>
+				</div>
 			</div>
 		</div><!-- /navbar-inner -->
-	</div>
-
-	<div class="search">
-		<img src="./img/gajgja.jpg" title="Evernote" />
-		<form class="form-search">
-			<div class="input-append">
-				<input type="text" class="span2 search-query">
-				<button type="submit" class="btn">Search</button>
-			</div>
-			<button type="button" class="btn btn-primary">新建笔记</buttom>
-		</form>
 	</div>
 
 	<div class="content">
@@ -80,8 +44,18 @@
             		<b class="caret" id="lcon2"></b>
             	</div>
             	<ul class="lister">
-            		<li class="list-style1"><a href="">所有个人笔记</a></li>
-            		<li class="list-style2"><a href="">垃圾箱</a></li>
+            		<li class="list-style1">
+            			<b class="book"></b>
+            			<a href="">所有个人笔记</a>
+            			<b class="borders">
+            				<b class="select">
+                  				<ul class="aid">
+                  					<li><a href="">编辑</a></li>
+                  					<li><a href="">删除</a></li>
+                  				</ul>
+                                    </b>
+            			</b>
+            		</li>
             	</ul>
             </div>
 		</div>
